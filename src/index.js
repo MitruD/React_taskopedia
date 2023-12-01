d387bf1f0e063163931d75a9977da4665675f405
Kronos <dimamitru@gmail.com>
2023-12-01 09:10:09 +0200

    Props and Components

--- src/index.js
+++ src/index.js
@@ -1,57 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import "./CSS/style.css";
+import Header from "./Header";
+import Student from "./Student";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello World!</div>);
-
-function MainHeader() {
-  return <h1 className='heading1'>REACT COURSE</h1>;
-}
-
-const subHeaderStyle={
-  color:"blueviolet",
-  backgroundColor:"lightgray",
-};
-
-function SubHeader() {
-  return <p style={subHeaderStyle}>This will be an exciting course.</p>;
-}
-
-function Header() {
-  return (
-    <div>
-      <MainHeader></MainHeader>
-      <SubHeader></SubHeader>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function MainBody() {
-
-  //var, let 
-const whatWeWillLearn = "React JS"
-
+  const whatWeWillLearn = "React JS";
+  const lectureCount = 3;
   return (
     <div>
-      <p>In this course, we will learn {whatWeWillLearn} by building TaskOPedia!</p>
+      <p>
+        In this course, we will learn {whatWeWillLearn} by building TaskOPedia!{" "}
+        <br />
+        Total Lecture - {lectureCount}
+      </p>
       <ul>
-        <li>basic Foundation</li>
-        <li>Functional and class components</li>
+        <li>Basic Foundation</li>
+        <li>Functional and Class Components</li>
       </ul>
+      {/* <div>
+        Enter Task :{" "}
+        <input maxLength={5} readOnly={false} placeholder="Ben"></input>
+      </div> */}
     </div>
   );
 }
 
-
 function Footer() {
-  return <p style = {{color:"gray", backgroundColor:"black"}}>Happy Coding!</p>;
+  return (
+    <p style={{ color: "gray", backgroundColor: "black" }}>Happy Coding!</p>
+  );
 }
 root.render(
-  <div>
+  <div className="container">
     <Header />
     <MainBody />
+    <div className="row">Students Enrolled</div>
+    <Student experience={2} name="Kris Walley" />
+    <Student experience={5} name="Angel Patrice" />
+    <Student experience={7} name="Rene Parker" />
     <Footer />
   </div>
 );
